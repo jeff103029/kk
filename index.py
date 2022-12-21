202b92842ee8a7f7a81525cf5513e6619bd47526
jeff103029 <jeff103029@gmail.com>
2022-12-21 23:10:08 +0800

    make it better

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, make_response, jsonify
 
 import firebase_admin
 from firebase_admin import credentials, firestore
-cred = credentials.Certificate("serviceAccountKey.json")
+cred = credentials.Certificate("serviceAccountKey3.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
